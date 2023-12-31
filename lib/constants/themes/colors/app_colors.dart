44897a62e099ffad85e5d0acc16456e4ecf0796f
alirezaseif20 <alirezaariye@gmail.com>
2023-12-31 21:44:04 +0330

    feat: add fonts and colors required by the app

---
+++ lib/constants/themes/colors/app_colors.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+@immutable
+class AppColors {
+  // Primary Colors
+  static const primary = Color(0xFFAEDC81);
+  static const primaryDark = Color(0xFF6CC51D);
+  static const primaryLight = Color(0xFFEBFFD7);
+
+  static const border = Color(0xFFEBEBEB);
+
+  static const background1 = Color(0xFFFFFFFF);
+  static const background2 = Color(0xFFF4F5F9);
+  static const background3 = Color(0xFFF5F5F5);
+  
+  static const text1 = Color(0xFFDADCE0);
+  static const text2 = Color(0xFF868889);
+
+  static const link = Color(0xFF1A0DAB);
+
+  const AppColors._();
+}
