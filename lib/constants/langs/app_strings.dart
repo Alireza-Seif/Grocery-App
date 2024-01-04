fe39b40f70d867ed1e53429235764e8d795ad6d8
alirezaseif20 <alirezaariye@gmail.com>
2024-01-04 20:43:30 +0330

    feat: Add some default text

---
+++ lib/constants/langs/app_strings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/foundation.dart' show immutable;
+
+@immutable
+class AppString {
+//Splash Screen
+  static const splashScreenText = '''Lorem ipsum dolor sit amet, consetetur 
+sadipscing elitr, sed diam nonumy''';
+
+// Auth
+  static const welcomeText = '''Lorem ipsum dolor sit amet, consetetur 
+sadipscing elitr, sed diam nonumy''';
+
+//Forgot Password
+  static const forgotPasswordText = '''Lorem ipsum dolor sit amet, consetetur 
+sadipscing elitr, sed diam nonumy''';
+
+
+}
