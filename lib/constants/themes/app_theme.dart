037e7bab83b82405c4529cc002dab80c673889b3
alirezaseif20 <alirezaariye@gmail.com>
2024-01-05 19:45:56 +0330

    feat: add text style at app_theme

---
+++ lib/constants/themes/app_theme.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MyTextStyles {
+  static const TextStyle titleStyle = TextStyle(
+    fontWeight: FontWeight.bold,
+    fontSize: 30,
+  );
+
+  static const TextStyle bodyText = TextStyle(
+    fontSize: 15,
+    fontWeight: FontWeight.w500,
+  );
+}
