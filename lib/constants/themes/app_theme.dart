2e87733798f567e83bf3641e17b842829298256c
alirezaseif20 <alirezaariye@gmail.com>
2024-01-06 16:22:19 +0330

    feat: Small change in the app theme

--- lib/constants/themes/app_theme.dart
+++ lib/constants/themes/app_theme.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'colors/app_colors.dart';
+
 class MyTextStyles {
   static const TextStyle titleStyle = TextStyle(
-    fontWeight: FontWeight.bold,
+    fontWeight: FontWeight.w700,
     fontSize: 30,
+    fontFamily: 'Poppins'
   );
 
   static const TextStyle bodyText = TextStyle(
     fontSize: 15,
     fontWeight: FontWeight.w500,
+    fontFamily: 'Poppins',
+    color: AppColors.text2,
   );
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'package:big_cart/screens/splash_1.dart';
+import 'package:big_cart/screens/splash/splash_1.dart';
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(
+    const MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const SplashScreen(),
+      home: SplashScreen1(),
     ),
   );
 }
