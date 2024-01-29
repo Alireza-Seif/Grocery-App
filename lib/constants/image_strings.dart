4198872946a515a8b6a27db54708701777381b4f
alirezaseif20 <alirezaariye@gmail.com>
2024-01-29 18:24:46 +0330

    refactor: Change the format of all images to webp

--- lib/constants/image_strings.dart
+++ lib/constants/image_strings.dart
@@ -2,21 +2,21 @@
 class MyImages{
 
 // Auth
-static String auth1 = 'assets/images/auth/auth1.png';
-static String auth2 = 'assets/images/auth/auth2.png';
-static String auth3 = 'assets/images/auth/auth3.png';
+static String auth1 = 'assets/images/auth/auth1.webp';
+static String auth2 = 'assets/images/auth/auth2.webp';
+static String auth3 = 'assets/images/auth/auth3.webp';
 
 // Splash
-static String bigCart = 'assets/images/splash/bigCart.png';
+static String bigCart = 'assets/images/splash/bigCart.webp';
 
-static String splash1 = 'assets/images/splash/splash1.png';
-static String splash2 = 'assets/images/splash/splash2.png';
-static String splash3 = 'assets/images/splash/splash3.png';
-static String splash4 = 'assets/images/splash/splash4.png';
+static String splash1 = 'assets/images/splash/splash1.webp';
+static String splash2 = 'assets/images/splash/splash2.webp';
+static String splash3 = 'assets/images/splash/splash3.webp';
+static String splash4 = 'assets/images/splash/splash4.webp';
 
 // Banners
-static String promoBanner1 = 'assets/images/banners/promo_banner_1.png';
+static String promoBanner1 = 'assets/images/banners/promo_banner_1.webp';
 static String promoBanner2 = 'assets/images/banners/promo_banner_2.webp';
-static String promoBanner3 = 'assets/images/banners/promo_banner_3.jpg';
-static String promoBanner4 = 'assets/images/banners/promo_banner_4.jpg';
+static String promoBanner3 = 'assets/images/banners/promo_banner_3.webp';
+static String promoBanner4 = 'assets/images/banners/promo_banner_4.webp';
 }
