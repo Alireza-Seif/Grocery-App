fc06cb309c5a6b13d80a5400b5b5b25f901fbac4
alirezaseif20 <alirezaariye@gmail.com>
2024-02-13 19:55:06 +0330

    fix: fix a bug for banner

---
+++ lib/widgets/home_widgets/promo_view.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class PromoView extends StatefulWidget {
+   PromoView({super.key, required PageController controller});
+
+    var controller;
+
+
+  @override
+  State<PromoView> createState() => _PromoViewState();
+}
+
+class _PromoViewState extends State<PromoView> {
+  var images = [
+    'assets/images/banners/promo_banner_1.webp',
+    'assets/images/banners/promo_banner_2.webp',
+    'assets/images/banners/promo_banner_3.webp',
+    'assets/images/banners/promo_banner_4.webp',
+  ];
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return PageView(
+      allowImplicitScrolling: true,
+      controller: widget.controller,
+      children: [
+        myPages(images[0]),
+        myPages(images[1]),
+        myPages(images[2]),
+        myPages(images[3]),
+      ],
+    );
+  }
+}
+
+Widget myPages(String image) {
+  return ClipRRect(
+    borderRadius: const BorderRadius.all(Radius.circular(30)),
+    child: Image(
+      image: AssetImage(image),
+      fit: BoxFit.fill,
+    ),
+  );
+}
